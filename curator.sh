489315f4c3c6a0aee74328ee1d73afb494d296be
Alan Raff <alanr917@hotmail.com>
2021-11-23 13:25:48 -0500

    remove if/else logic and unneeded functions

--- curator.sh
+++ curator.sh
@@ -4,21 +4,8 @@
 model=$1
 image_uri=$2
 
-generate_usdz () {
-	uri_hash=$(echo -n $image_uri |openssl base64 -A)
-	curl -s $image_uri -o models/$model/tmp/$uri_hash.png
-	cp models/$model/tmp/$uri_hash.png models/$model/nft.png
-	usdz_filename=models/"$model"/exports/"$uri_hash".usdz
-	lib/usdzconvert/usdzconvert models/$model/$model.fbx $usdz_filename
-	rm -rf models/$model/tmp/$uri_hash.png
-}
-
-if [ $model = "wood_block" ]
-then
-	generate_usdz
-elif [ $model = "easel" ]
-then
-    generate_usdz
-else
-    echo "Failed"
-fi
+uri_hash=$(echo -n $image_uri |openssl base64 -A)
+curl -s $image_uri -o models/$model/tmp/$uri_hash.png
+cp models/$model/tmp/$uri_hash.png models/$model/nft.png
+lib/usdzconvert/usdzconvert models/$model/$model.fbx models/$model/exports/$uri_hash.usdz
+rm -rf models/$model/tmp/$uri_hash.png
