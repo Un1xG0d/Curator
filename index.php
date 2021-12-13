36f17a5757d27f6563de1ad2c142ae1aec7e3918
Alan Raff <alanr917@hotmail.com>
2021-12-13 01:49:15 -0500

    change button option to treasure chest

--- index.php
+++ index.php
@@ -27,13 +27,13 @@
         <img class="thumbnail_image" src="img/thumbnail_easel.png">
         <div>Easel</div>
       </button>
-      <button type="submit" name="model" id="model" value="glass_case">
-        <img class="thumbnail_image" src="img/thumbnail_placeholder.png">
-        <div>Glass display case</div>
-      </button>
       <button type="submit" name="model" id="model" value="picture_frame">
         <img class="thumbnail_image" src="img/thumbnail_picture_frame.png">
         <div>Picture frame</div>
+      </button>
+      <button type="submit" name="model" id="model" value="treasure_chest">
+        <img class="thumbnail_image" src="img/thumbnail_placeholder.png">
+        <div>Treasure chest</div>
       </button>
       <button type="submit" name="model" id="model" value="wood_block">
         <img class="thumbnail_image" src="img/thumbnail_wood_block.png">
