da1dd9927a7101d64fd78ef96428b3ddce6ee85a
Alan Raff <alanr917@hotmail.com>
2022-02-13 03:53:00 -0500

    remove lib/ directory to refresh language detection

--- lib/install_sip.sh
+++
@@ -1,6 +0,0 @@
-tar -xvf lib/sip-4.19.3.tar.gz -C lib/
-cd lib/sip-4.19.3/
-python configure.py
-make
-make install
-cd /var/www/html/
--- lib/set_env_vars.sh
+++
@@ -1,7 +0,0 @@
-echo "export PATH=/usr/local/USD/bin:/var/www/html/lib/usdzconvert:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> /var/www/html/lib/.bashrc
-echo "export PYTHONPATH=/usr/local/lib/python2.7/dist-packages:/usr/local/USD/lib/python" >> /var/www/html/lib/.bashrc
-echo "export LD_LIBRARY_PATH=/var/www/html/lib/fbxsdk/lib/gcc/x64/release" >> /var/www/html/lib/.bashrc
-echo "export SIP_ROOT=/var/www/html/lib/sip-4.19.3" >> /var/www/html/lib/.bashrc
-echo "export FBXSDK_ROOT=/var/www/html/lib/fbxsdk" >> /var/www/html/lib/.bashrc
-echo "export QUOTING_STYLE=literal" >> /var/www/html/lib/.bashrc
-. /var/www/html/lib/.bashrc
